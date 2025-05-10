4745e01ad9d202fc70ec9e73143b96ba23189249
Daniel <thecodedaniel@gmail.com>
2025-05-10 07:52:22 +0100

    feat: generate image implementation

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,10 +1,7 @@
 import 'dart:developer';
-import 'dart:io';
 
 import 'package:firebase_vertexai/firebase_vertexai.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -18,25 +15,20 @@ class _HomeViewState extends State<HomeView> {
   final String generativeAIkey = String.fromEnvironment('GENERATIVE_AI_API_KEY');
 
   // Late instance of generative AI model
-  late final GenerativeModel model;
+  late final ImagenModel model;
 
   // AI model reply
-  String aiReply = '';
+  List aiReply = [];
 
   // Text prompt query
   final queryController = TextEditingController();
-
-  // Nullable selected image
-  String? selectedImagePath;
 
   // Loading status boolean
   bool isLoading = false;
 
   @override
   void initState() {
-    model = FirebaseVertexAI.instance.generativeModel(
-      model: 'gemini-2.0-flash-001',
-    );
+    model = FirebaseVertexAI.instance.imagenModel(model: 'imagen-3.0-generate-002');
     super.initState();
   }
 
@@ -57,25 +49,22 @@ class _HomeViewState extends State<HomeView> {
                   controller: queryController,
                   decoration: InputDecoration(
                     labelText: "Please enter a text to query the model",
-                    suffixIcon: IconButton(
-                      onPressed: () => showModalBottomSheet(
-                        context: context,
-                        builder: (context) => selectImageSheet(context),
-                      ),
-                      icon: selectedImagePath != null
-                          ? CircleAvatar(
-                              backgroundImage: FileImage(File(selectedImagePath!)),
-                              child: Icon(Icons.edit, color: Colors.white),
-                            )
-                          : Icon(Icons.add_a_photo_outlined),
-                    ),
                   ),
                 ),
                 TextButton(
                   onPressed: () => queryController.text.isEmpty ? null : readyAiModel(),
                   child: isLoading ? CircularProgressIndicator.adaptive() : Text("Click to query"),
                 ),
-                Text(aiReply),
+                ...List.generate(
+                  aiReply.length,
+                  (index) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 100,
+                      decoration: BoxDecoration(image: DecorationImage(image: MemoryImage(aiReply[index]))),
+                    );
+                  },
+                ),
               ],
             ),
           ),
@@ -87,23 +76,15 @@ class _HomeViewState extends State<HomeView> {
   readyAiModel() async {
     setState(() => isLoading = true);
     try {
-      final image = selectedImagePath != null ? await File(selectedImagePath!).readAsBytes() : null;
+      final response = await model.generateImages(queryController.text);
 
-      final content = [
-        // Content.text(queryController.text), // uncomment this code if you only want to query for text
-        Content.multi([
-          TextPart(queryController.text),
-          if (selectedImagePath != null) InlineDataPart("image/jpeg", image!),
-        ])
-      ];
+      final imageList = response.images;
 
-      final response = await model.generateContent(content);
-
-      final text = response.text;
-
-      setState(() {
-        aiReply = text ?? 'Oops... Could not connect with AI';
-      });
+      for (var img in imageList) {
+        setState(() {
+          aiReply.add(img.bytesBase64Encoded);
+        });
+      }
 
       // end of try catch block
       setState(() => isLoading = false);
@@ -114,80 +95,5 @@ class _HomeViewState extends State<HomeView> {
     } finally {
       setState(() => isLoading = false);
     }
-  }
-
-  generateImage() {
-    try {} on Exception catch (e) {
-      inspect(e);
-      log(e.toString());
-    } finally {}
-  }
-
-  Future<File?> pickImage(ImageSource source) async {
-    try {
-      final ImagePicker picker = ImagePicker();
-
-      // 🔹 Check & Request Permissions (Only needed for Camera on Android & iOS)
-      if (source == ImageSource.camera) {
-        PermissionStatus cameraPermission = await Permission.camera.request();
-
-        if (cameraPermission.isDenied) {
-          throw Exception("Camera permission is required to take pictures.");
-        } else if (cameraPermission.isPermanentlyDenied) {
-          throw Exception("Camera permission is permanently denied. Enable it in settings.");
-        }
-      }
-
-      // 🔹 Pick Image
-      XFile? pickedFile = await picker.pickImage(source: source);
-
-      // 🔹 Handle No Image Selected
-      if (pickedFile == null) {
-        throw Exception("No image selected.");
-      }
-
-      setState(() => aiReply = '');
-      queryController.clear();
-
-      return File(pickedFile.path); // ✅ Return the File
-    } catch (e) {
-      log("Image Picker Error: $e");
-      return null; // 🚨 Return null if any error occurs
-    }
-  }
-
-  // Select option for image query
-  Widget selectImageSheet(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListTile(
-            leading: Icon(Icons.file_copy_outlined),
-            title: Text("Gallery"),
-            onTap: () async {
-              Navigator.pop(context);
-              final image = await pickImage(ImageSource.gallery);
-              setState(() => selectedImagePath = image?.path);
-            },
-          ),
-          Divider(),
-          ListTile(
-            leading: Icon(Icons.camera_alt_outlined),
-            title: Text("Camera"),
-            onTap: () async {
-              Navigator.pop(context);
-              final image = await pickImage(ImageSource.camera);
-              setState(() => selectedImagePath = image?.path);
-            },
-          ),
-        ],
-      ),
-    );
   }
 }
